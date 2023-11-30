9542034d3cf13131cc7b39bc86e358ff0d0f94d0
Simon Jones <github@simonjjones.com>
2023-11-30 13:50:39 -0500

    feat: adding shell.nix

---
+++ shell.nix
@@ -0,0 +1,18 @@
+let
+  nixpkgs = import (builtins.fetchGit {
+    name = "nixos-unstable-2023-10-18";
+    url = https://github.com/nixos/nixpkgs/;
+    ref = "refs/heads/nixos-unstable";
+    rev = "ca012a02bf8327be9e488546faecae5e05d7d749";
+  }) {};
+  shellname = "thesis";
+in
+  nixpkgs.mkShell {
+    buildInputs = [
+      nixpkgs.tectonic
+      nixpkgs.pandoc
+    ];
+    shellHook = ''
+      alias pdf="pandoc --defaults pdf.yaml --to latex --metadata-file config.yaml --lua-filter .filters/abstract-to-meta.lua --template template/thesis.tex"
+    '';
+  }
